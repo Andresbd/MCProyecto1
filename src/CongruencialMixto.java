b3e6429716132c50893c8ee2d877cf4fbd55b212
Manuel Avalos Tovar <manuelavalos97@hotmail.es>
2019-10-03 17:08:08 -0500

    Congruencial Mixto

--- src/CongruencialMixto.java
+++ src/CongruencialMixto.java
@@ -1,2 +1,101 @@
+import java.util.ArrayList;
+
 public class CongruencialMixto {
+    public int[] GenMix(int X0, int a, int c, int m, int n){
+        int[] aux = new int[n];
+
+        int result = X0;
+        for (int i = 0; i < n; i++){
+            aux[i] = GenMix(result, a, c, m);
+            result = aux[i];
+        }
+
+        return aux;
+    }
+
+    public int GenMix(int X0, int a, int c, int m){
+        //if we meet the conditions we do the whole process, else we return -1
+        if(checkConditions(a, c, m)){
+            int result = ((a * X0) + c) % m;
+            return result;
+        }
+        return -1;
+    }
+
+    //if conditions are true, it returns true
+    private boolean checkConditions(int a, int c, int m){
+
+        if (first(c, m) && second(m, a) && third(m, a)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean first(int c, int m){
+        if(mcd(c, m) == 1){
+            return true;
+        }
+        return false;
+    }
+
+    //checar este
+    private boolean second(int m, int a){
+        ArrayList primes = primeFactors(m);
+        ArrayList aux = new ArrayList();
+
+        for (int i = 0; i< primes.size(); i++){
+            aux.add((a-1)%(int)primes.get(i));
+            if((int)aux.get(i) == 0){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean third(int m, int a){
+        if(m%4 == 0 && (a-1)%4 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    private int mcm(int num1, int num2){
+        int mcm = 0;
+        int a = Math.max(num1, num2);
+        int b = Math.min(num1, num2);
+        mcm = (a/mcd(a,b))*b;
+        return mcm;
+    }
+
+    private int mcd(int num1, int num2){
+        int mcd = 0;
+        int a = Math.max(num1, num2);
+        int b = Math.min(num1, num2);
+        do {
+            mcd = b;
+            b = a%b;
+            a = mcd;
+        } while(b!=0);
+        return mcd;
+    }
+
+    private ArrayList primeFactors(int n){
+        int i = 2;
+        ArrayList factors = new ArrayList();
+
+        while(i*i <= n){
+            if(n%i == 1){
+                i ++;
+            }
+            else {
+                n/=i;
+                factors.add(i);
+            }
+        }
+        if (n>1){
+            factors.add(n);
+        }
+        return factors;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
     // write your code here
 
-    int result;
+        int result;
 
         MetodosCuadrados MC = new MetodosCuadrados();
         GeneradorMultiplicativo GM = new GeneradorMultiplicativo();
+        CongruencialMixto CM = new CongruencialMixto();
 
         result = MC.GenerarMC(3547);
         System.out.println("Iteración 0: "+result);
@@ -14,6 +15,12 @@ public class Main {
         for(int i = 1; i < 7; i++) {
             result = MC.GenerarMC(result);
              System.out.println("Iteración " + i + ": "+result);
+        }
+
+        int[] aux = CM.GenMix(37, 9, 29, 100, 10 );
+
+        for (int i = 0; i<aux.length; i++){
+            System.out.println(aux[i]);
         }
     }
 }
