9cddb6630bd02cfce27045a507270cc8f79dca6c
Andrés Bustamante <andres.bd@outlook.com>
2019-10-09 11:32:26 -0500

    Mejoras sobre MC y codigo de CL

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,29 @@ public class Main {
     public static void main(String[] args) {
     // write your code here
 
-        int result;
-
         MetodosCuadrados MC = new MetodosCuadrados();
         GeneradorMultiplicativo GM = new GeneradorMultiplicativo();
         CongruencialMixto CM = new CongruencialMixto();
+        CongruencialLineal CL = new CongruencialLineal();
 
-        result = MC.GenerarMC(3547);
-        System.out.println("Iteración 0: "+result);
-
-        for(int i = 1; i < 7; i++) {
-            result = MC.GenerarMC(result);
-             System.out.println("Iteración " + i + ": "+result);
+        MC.GenerarMC(3708, 8);
+        
+        for(int x = 0; x < MC.MCAleatorio.size(); x++) {
+            System.out.println("Aleatorio: "+MC.MCAleatorio.get(x));
+            System.out.println("RI: "+MC.MCRandomRi.get(x));
         }
 
-        int[] aux = CM.GenMix(37, 9, 29, 100, 10 );
+        int[] aux = CM.GenMix(4, 5, 7, 8, 7);
 
         for (int i = 0; i<aux.length; i++){
             System.out.println(aux[i]);
+        }
+
+        CL.CongLineal(4, 5, 7, 8, 5);
+        
+        for(int y = 0; y < CL.CLAleatorio.size(); y++) {
+            System.out.println("Aleatorio: "+CL.CLAleatorio.get(y));
+            System.out.println("RI: "+CL.CLRandomRi.get(y));
         }
     }
 }
