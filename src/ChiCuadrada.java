3ce2464cc7e7442fdce44712ffad0222541e64d3
Andrés Bustamante <andres.bd@outlook.com>
2019-10-09 20:07:41 -0500

    More advancements on CHI, Integration missing

--- src/ChiCuadrada.java
+++ src/ChiCuadrada.java
@@ -4,10 +4,15 @@ import java.util.Collections;
 public class ChiCuadrada {
 
     int n;
-    float min, max, lambda, k, sum, median, interval;
+    double min, max, lambda, k, sum, median, interval;
+    double FA[];
+    double FR[];
+    ArrayList<Double> intervales = new ArrayList<Double>();
 
     public void ChiTest(ArrayList<Float> values) {
+
         n = values.size();
+        k = 1 + 3.222 * Math.log10(n);
 
         max = Collections.max(values);
         min = Collections.min(values);
@@ -19,5 +24,43 @@ public class ChiCuadrada {
         median = sum / n;
         lambda = 1/median;
         interval = (max-min)/k;
+
+        GenerateIntervals();
+
+        AbsoluteRelative(values, intervales);
+        
+    }
+
+    void GenerateIntervals() {
+        double base = interval;
+
+        while (interval < 1) {
+            intervales.add(interval);
+            interval += base;
+        }
+
+        interval += base;
+        intervales.add(interval);
+    }
+
+    void AbsoluteRelative(ArrayList<Float> data, ArrayList<Double> seccions) {
+        FA = new double[seccions.size()];
+
+        for (int x = 0; x < seccions.size(); x++) {
+            int cont = 0;
+            for(int y = 0; y < data.size(); y++) {
+                if(data.get(y) < seccions.get(x)) {
+                    data.remove(y);
+                    cont++;
+                }
+            }
+            FA[x] = cont;
+        }
+
+        FR = new double[seccions.size()];
+
+        for(int z = 0; z < seccions.size(); z++){
+            FR[z] = FA[z] / n;
+        }
     }
 }
